b514917e4615d5ab9d4091a6a9c3512c86f02cd3
Advit Soni <zkc08@outlook.com>
2020-08-29 01:30:58 +0530

    Using something i don't remember it's name 🤣

---
+++ Source/PuzzlePlatforms/PuzzlePlatformGameInstance.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PuzzlePlatformGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API UPuzzlePlatformGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UPuzzlePlatformGameInstance(const FObjectInitializer & ObjectInitializer);
+
+	virtual void Init();
+	
+};
